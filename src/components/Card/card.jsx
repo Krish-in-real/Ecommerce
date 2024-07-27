50762b965666a0f32ec9d73e2f2e47f6727f076b
ranitamaity <ranitamaity.aeie2021@nsec.ac.in>
2024-07-27 16:24:10 +0530

    fixed card,add-to-cart

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './card.css'
 import { AiFillStar } from 'react-icons/ai';
 import { AiOutlineStar } from "react-icons/ai";
@@ -6,12 +6,25 @@ import { AiOutlineStar } from "react-icons/ai";
 
 
 function Card({id,imgSrc,title,price,discount}) {
+  const [hidden,sethidden] = useState(true);
           return (
-            <div key={id} className="e-card">
+            <div className="down">
+            <div key={id} 
+            className="e-card"
+            onMouseEnter={() => sethidden (false)}
+            onMouseLeave={() => sethidden (true)}
+            >
               <div className="image">
                 <img className="card-image" src={imgSrc} alt="" />
               </div>
-              <div className="desc">
+              
+              <div className="add-to-cart">{
+                hidden?null:<button className="abs">Add To Cart</button>
+              }
+                
+             </div>
+             </div>
+             <div className="desc">
                 <h4>{title}</h4>
                 <span>${price}</span>
               </div>
